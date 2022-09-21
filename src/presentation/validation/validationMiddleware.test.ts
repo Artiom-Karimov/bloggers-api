bfc497c9c2d2cbbb9f8dd97cb7e4410766800a7c
Artiom Karimov <lunatic0259@gmail.com>
2022-09-21 17:19:39 +0300

    fixed error duplication in APIErrorResult

---
+++ src/presentation/validation/validationMiddleware.test.ts
@@ -0,0 +1,15 @@
+import { expect, test} from '@jest/globals';
+import { APIErrorResult } from './apiErrorResultFormatter';
+import { removeDuplicates } from './validationMiddleware';  
+
+describe('validation logic tests', () => {
+    it('removeDuplicates should remove duplicates', () => {
+        const duplicatedErrors = new APIErrorResult([
+            { message:'bubble gum', field: 'gubble bum' },
+            { message:'bubble gum', field: 'gubble bum' },
+            { message:'bubble gum', field: 'torture' },
+        ])
+        const result = removeDuplicates(duplicatedErrors)
+        expect(result.errorsMessages.length).toBe(2)
+    })
+})
--- src/presentation/validation/validationMiddleware.ts
+++ src/presentation/validation/validationMiddleware.ts
@@ -1,8 +1,19 @@
 import { NextFunction, Request, Response } from "express"
-import { formatErrors } from "./apiErrorResultFormatter"
+import { APIErrorResult, formatErrors } from "./apiErrorResultFormatter"
+
+export const removeDuplicates = (errors:APIErrorResult): APIErrorResult => {
+    const result = new APIErrorResult([])
+    errors.errorsMessages.forEach((e) => {
+        if(!result.errorsMessages.find((el) => el.field === e.field))
+            result.errorsMessages.push(e)
+    })
+    return result
+}
 
 export const validationMiddleware = (req:Request,res:Response,next:NextFunction) => {
     const errors = formatErrors(req)
     if(errors.isEmpty()) next()
-    else res.status(400).send(errors)
+    else {
+        res.status(400).send(removeDuplicates(errors))
+    }
 }
