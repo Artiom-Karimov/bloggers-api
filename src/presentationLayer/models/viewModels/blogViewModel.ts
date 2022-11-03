7e83c5f349e01dd66f89045e8d6adb1f5a6383cc
Artiom Karimov <lunatic0259@gmail.com>
2022-11-03 18:37:28 +0300

    minor refactoring

--- src/presentationLayer/models/viewModels/blogViewModel.ts
+++ src/presentationLayer/models/viewModels/blogViewModel.ts
@@ -1,13 +1,8 @@
 export default class BlogViewModel {
-    public id: string
-    public name: string
-    public youtubeUrl: string
-    public createdAt: string
-
-    constructor(id:string,name:string,youtubeUrl:string,createdAt:string) {
-        this.id = id
-        this.name = name
-        this.youtubeUrl = youtubeUrl
-        this.createdAt = createdAt
+    constructor(
+        public id: string,
+        public name: string,
+        public youtubeUrl: string,
+        public createdAt: string) {
     }
 }
